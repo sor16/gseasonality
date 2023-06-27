46c3f188b9d9de95a88fb865415d806e5e970673
Sölvi Rögnvaldsson <solviro@gmail.com>
2023-06-27 15:10:45 +0300

    added the option to add individual-level data to seasonality_phenotype function

--- R/seasonality_phenotype.R
+++ R/seasonality_phenotype.R
@@ -2,8 +2,15 @@
 #'
 #' Based on an input model, this function calculates the seasonality phenotype for each individual
 #' @param mod object of type "seasm"
+#' @param data Optional data.frame with columns:
+#'                    \itemize{
+#'                       \item{"ID"}{ a character denoting the ID of the patient}
+#'                       \item{"EVENT_DATE"}{ character in the format YYYY-MM-DD denoting the first date the disease is recorded in the registry}
+#'                    }
+#'              Useful if mod was fitted on an external data set.
 #' @param year_start integer denoting the starting year of the analysis
 #' @param year_end integer denoting the final year of the analysis
+#' @param adjusted boolean denoting whether to perform specific adjustments for months with low hospital usage (July and December). Defaults to FALSE.
 #' @param ... Not used for this function
 #' @details Details here
 #' @return The function returns a tibble containing two seasonality phenotype:
@@ -18,8 +25,32 @@
 #' @importFrom magrittr "%>%"
 #' @importFrom stats approx qnorm
 #' @export
-get_seasonality_phenotype <- function(mod,year_start=NULL,year_end=NULL){
+get_seasonality_phenotype <- function(mod,data=NULL,year_start=NULL,year_end=NULL,adjusted=F){
   stopifnot(inherits(mod,'seasm'))
+  if(!is.null(data)){
+    stopifnot(inherits(data,'data.frame'))
+    if(!all(c('ID','EVENT_DATE') %in% names(data))){
+      stop('data must include columns ID and EVENT_DATE. Check the help page for more details by writing ?get_seasonality_phenotype')
+    }
+    if(length(unique(data$ID)) != nrow(data)){
+      stop('There must be exactly one entry for each ID. Check the help page for more details by writing ?get_seasonality_phenotype')
+    }
+    if(suppressWarnings(any(is.na(ymd(data$EVENT_DATE))))){
+      stop('EVENT_DATE column in data must be in the format YYYY-MM-DD. Check the help page for more details by writing ?get_seasonality_phenotype')
+    }
+    if(!is.null(mod$data)){
+      warning('Since data is provided, it will be used instead of the data in the seasonality model object.')
+    }
+    pheno_dat <- mutate(data,EVENT_DATE=ymd(EVENT_DATE)) %>%
+                 mutate(EVENT_YEAR=year(EVENT_DATE),
+                        EVENT_MONTH=month(EVENT_DATE),
+                        EVENT_DAY=day(EVENT_DATE))
+  }else{
+    if(is.null(mod$data)){
+      stop('No individual-level data has been provided. Can either be included in mod or provided using the data argument. For more information, write ?seasonality_phenotype')
+    }
+    pheno_dat <- mod$data
+  }
   if(is.null(year_start)){
     year_start <- mod$year_start
   }
@@ -33,16 +64,12 @@ get_seasonality_phenotype <- function(mod,year_start=NULL,year_end=NULL){
   if(year_end<=year_start){
     stop('year_end must be strictly greater than year_start')
   }
-  dates <- seq(ymd(paste0(year_start,'-01-01')),ymd(paste0(year_end,'-12-31')),by='1 day')
-  num_days_months <- tibble(year=year(dates),month=month(dates),day=day(dates)) %>%
-                     group_by(year,month) %>%
-                     summarise(num_days=max(day),.groups='drop')
-  pheno_dat <- mod$data
+  month_size_dat <- get_month_size(year_start,year_end)
   #Convert date to month number
-  pheno_dat <- inner_join(pheno_dat,num_days_months,by=c('EVENT_YEAR'='year','EVENT_MONTH'='month')) %>%
-               mutate(EVENT_MONTH_DEC=EVENT_MONTH + EVENT_DAY/num_days) %>%
+  pheno_dat <- inner_join(pheno_dat,month_size_dat,by=c('EVENT_YEAR','EVENT_MONTH')) %>%
+               mutate(EVENT_MONTH_DEC=EVENT_MONTH + EVENT_DAY/nr_days) %>%
                mutate(EVENT_MONTH_DEC=ifelse(EVENT_MONTH_DEC>12.5,EVENT_MONTH_DEC-12,EVENT_MONTH_DEC))
-  month_grid <- get_grid(type='seasonal',year_start=year_start,year_end=year_end)
+  month_grid <- get_grid(type='seasonal',year_start=year_start,year_end=year_end,adjustment=ifelse(mod$adjusted,'binary',''))
   seasonal_smooth_term <- predict(mod$fit,newdata=month_grid,type='terms',unconditional=T,se.fit=T)
   pheno_dat <- mutate(pheno_dat,seasonal_val=approx(x=month_grid$EVENT_MONTH,
                                                     y=seasonal_smooth_term$fit[,'s(EVENT_MONTH)'],
